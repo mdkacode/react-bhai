bb7c7e9d8f75b678bdb0ff6ee8616df95bf3a02f
hmm-mayank <mayankanildwivedi@gmail.com>
2020-07-17 23:38:43 +0530

    Class component implementation

--- src/App.js
+++ src/App.js
@@ -3,13 +3,39 @@ import UserInputContainer from "./container/userInput.container";
 import QuestionList from "./container/questionList"
 import './App.css';
 
-function App() {
-  return (
-    <>
-    <UserInputContainer/>
-    <QuestionList/>
-    </>
-  );
+class App extends React.Component {
+
+  constructor(props) {
+    super(props)
+    this.state=  {
+      name:"Mayank"
+    }
+    console.log("I will call First"); // this will call first
+  }
+ 
+  static getDerivedStateFromProps () {
+    console.log('SECOND CALL');  // this will call second
+
+     return {name :"Anmol"}
+     console.log('aasas');
+  }
+
+  componentDidMount () {
+    this.setState({
+      name:"priyam" //
+    })
+    console.log('Third CALL');  // this will call second
+  }
+ 
+  render() {
+    return (
+      <>
+        <UserInputContainer/>
+        <QuestionList />
+      </>
+    );
+  }
 }
 
 export default App;
+
--- src/index.js
+++ src/index.js
@@ -9,9 +9,8 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+ 
+    <App />,
   document.getElementById('root')
 );
 
